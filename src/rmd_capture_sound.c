7b7c395f77c7b809e85e27f45c54cabd1e8baea1
Alexey Min <alexey.min@gmail.com>
2015-10-28 00:56:20 +0500

    rmd_capture_sound: can capture from pulseaudio.

--- src/rmd_capture_sound.c
+++ src/rmd_capture_sound.c
@@ -40,44 +40,62 @@ void *CaptureSound(ProgData *pdata){
 
 #ifdef HAVE_LIBASOUND
     int frames=pdata->periodsize;
+    // ^^ approximate period size in frames
 #endif
     //start capturing only after first frame is taken
     usleep(pdata->frametime);
 
-    while(pdata->running){
+    // while recording, loop
+    while(pdata->running) {
         int sret=0;
         SndBuffer *newbuf,*tmp;
         if (pdata->paused) {
 #ifdef HAVE_LIBASOUND
-            if(!pdata->hard_pause){
-                snd_pcm_pause(pdata->sound_handle,1);
+#ifdef HAVE_LIBPULSE_SIMPLE
+            if( pdata->using_pulseaudio ) {
+                // compiled with PA support AND actually using PA
+                // just wait on mutex, don't do anything
                 pthread_mutex_lock(&pdata->pause_mutex);
                 pthread_cond_wait(&pdata->pause_cond, &pdata->pause_mutex);
                 pthread_mutex_unlock(&pdata->pause_mutex);
-                snd_pcm_pause(pdata->sound_handle,0);
+                // discard any data in audio buffer
+                int pa_error = 0;
+                pa_simple_flush(pdata->paconn_handle, &pa_error);
             }
-            else{//device doesn't support pause(is this the norm?mine doesn't)
-                snd_pcm_close(pdata->sound_handle);
-                pthread_mutex_lock(&pdata->pause_mutex);
-                pthread_cond_wait(&pdata->pause_cond, &pdata->pause_mutex);
-                pthread_mutex_unlock(&pdata->pause_mutex);
-                pdata->sound_handle=
-                    OpenDev(pdata->args.device,
-                            &pdata->args.channels,
-                            &pdata->args.frequency,
-                            &pdata->args.buffsize,
-                            NULL,
-                            NULL,
-                            NULL//let's hope that the device capabilities
-                                //didn't magically change
-                            );
-                if(pdata->sound_handle==NULL){
-                    fprintf(stderr,"Couldn't reopen sound device.Exiting\n");
-                    pdata->running = FALSE;
-                    errno=3;
-                    pthread_exit(&errno);
+#endif
+            if( !pdata->using_pulseaudio ) {
+                if( !pdata->hard_pause ) {
+                    snd_pcm_pause(pdata->sound_handle,1);
+                    pthread_mutex_lock(&pdata->pause_mutex);
+                    pthread_cond_wait(&pdata->pause_cond, &pdata->pause_mutex);
+                    pthread_mutex_unlock(&pdata->pause_mutex);
+                    snd_pcm_pause(pdata->sound_handle,0);
+                } else {
+                    // device doesn't support pause(is this the norm?mine doesn't)
+                    // if sound device doesn't support pause
+                    // we have to close and reopen
+                    snd_pcm_close(pdata->sound_handle);
+                    pthread_mutex_lock(&pdata->pause_mutex);
+                    pthread_cond_wait(&pdata->pause_cond, &pdata->pause_mutex);
+                    pthread_mutex_unlock(&pdata->pause_mutex);
+                    pdata->sound_handle=
+                        OpenDev(pdata->args.device,
+                                &pdata->args.channels,
+                                &pdata->args.frequency,
+                                &pdata->args.buffsize,
+                                NULL,
+                                NULL,
+                                NULL//let's hope that the device capabilities
+                                    //didn't magically change
+                                );
+                    if(pdata->sound_handle==NULL){
+                        fprintf(stderr,"Couldn't reopen sound device.Exiting\n");
+                        pdata->running = FALSE;
+                        errno=3;
+                        pthread_exit(&errno);
+                    }
                 }
-            }
+            } // not using pulseaudio
 #else
             close(pdata->sound_handle);
             pthread_mutex_lock(&pdata->pause_mutex);
@@ -98,33 +116,62 @@ void *CaptureSound(ProgData *pdata){
 
         //create new buffer
         newbuf=(SndBuffer *)malloc(sizeof(SndBuffer));
+        newbuf->data = NULL;
+        size_t newbuf_datasize = 0;
 #ifdef HAVE_LIBASOUND
-        newbuf->data=(signed char *)malloc(frames*pdata->sound_framesize);
+        if( !pdata->using_pulseaudio) {
+            // using pure alsa
+            newbuf->data=(signed char *)malloc(frames*pdata->sound_framesize);
+            newbuf_datasize = frames*pdata->sound_framesize;
+            newbuf->datasize = newbuf_datasize;
+        } else {
+            // for pulseaudio allocate buffer as for #else branch
+            newbuf->data=(signed char *)malloc((pdata->args.buffsize << 1) * pdata->args.channels);
+            newbuf_datasize = (pdata->args.buffsize << 1) * pdata->args.channels;
+            newbuf->datasize = newbuf_datasize;
+        }
 #else
         newbuf->data=(signed char *)malloc(((pdata->args.buffsize<<1)*
                                             pdata->args.channels));
+        newbuf_datasize = (pdata->args.buffsize << 1) * pdata->args.channels;
+        newbuf->datasize = newbuf_datasize;
 #endif
         newbuf->next=NULL;
 
         //read data into new buffer
 #ifdef HAVE_LIBASOUND
-        while(sret<frames){
-            int temp_sret=snd_pcm_readi(pdata->sound_handle,
-                                newbuf->data+pdata->sound_framesize*sret,
-                                frames-sret);
-            if(temp_sret==-EPIPE){
-                fprintf(stderr,"%s: Overrun occurred.\n",
-                               snd_strerror(temp_sret));
-                snd_pcm_prepare(pdata->sound_handle);
+#ifdef HAVE_LIBPULSE_SIMPLE
+        if( pdata->using_pulseaudio ) {
+            int pa_error = 0;
+            int ret = pa_simple_read(pdata->paconn_handle, newbuf->data,
+                    newbuf_datasize, &pa_error);
+            if( ret < 0 ) {
+                fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(pa_error));
+                pdata->running = FALSE;
             }
-            else if (temp_sret<0){
-                fprintf(stderr,"An error occured while reading sound data:\n"
-                               " %s\n",
-                               snd_strerror(temp_sret));
-                snd_pcm_prepare(pdata->sound_handle);
+        }
+#endif
+        if( !pdata->using_pulseaudio) {
+            // using pure alsa
+            // read interleaved PCM data until `frames` num frames read
+            while(sret<frames){
+                int temp_sret=snd_pcm_readi(pdata->sound_handle,
+                                    newbuf->data+pdata->sound_framesize*sret,
+                                    frames-sret);
+                if(temp_sret==-EPIPE){
+                    fprintf(stderr,"%s: Overrun occurred.\n",
+                                   snd_strerror(temp_sret));
+                    snd_pcm_prepare(pdata->sound_handle);
+                }
+                else if (temp_sret<0){
+                    fprintf(stderr,"An error occured while reading sound data:\n"
+                                   " %s\n",
+                                   snd_strerror(temp_sret));
+                    snd_pcm_prepare(pdata->sound_handle);
+                }
+                else
+                    sret+=temp_sret;
             }
-            else
-                sret+=temp_sret;
         }
 #else
         sret=0;
@@ -145,31 +192,38 @@ void *CaptureSound(ProgData *pdata){
         }while(sret<((pdata->args.buffsize<<1)*
                      pdata->args.channels));
 #endif
-        //queue the new buffer
-        pthread_mutex_lock(&pdata->sound_buffer_mutex);
-        tmp=pdata->sound_buffer;
-        if(pdata->sound_buffer==NULL)
-                pdata->sound_buffer=newbuf;
-        else{
-            while(tmp->next!=NULL)
-                tmp=tmp->next;
-            tmp->next=newbuf;
+        // queue the new buffer
+        pthread_mutex_lock( &pdata->sound_buffer_mutex );
+        tmp = pdata->sound_buffer;
+        if( pdata->sound_buffer == NULL ) {
+            pdata->sound_buffer = newbuf;
+        } else {
+            // go by the linked list till the end
+            while( tmp->next != NULL )
+                tmp = tmp->next;
+            tmp->next = newbuf;
         }
-        pthread_mutex_unlock(&pdata->sound_buffer_mutex);
-
+        pthread_mutex_unlock( &pdata->sound_buffer_mutex );
 
         //signal that there are data to be proccessed
-        pthread_mutex_lock(&pdata->snd_buff_ready_mutex);
-        pthread_cond_signal(&pdata->sound_data_read);
-        pthread_mutex_unlock(&pdata->snd_buff_ready_mutex);
+        pthread_mutex_lock( &pdata->snd_buff_ready_mutex );
+        pthread_cond_signal( &pdata->sound_data_read );
+        pthread_mutex_unlock( &pdata->snd_buff_ready_mutex );
     }
 #ifdef HAVE_LIBASOUND
-    snd_pcm_close(pdata->sound_handle);
+#ifdef HAVE_LIBPULSE_SIMPLE
+    if( pdata->using_pulseaudio ) {
+        if( pdata->paconn_handle ) {
+            pa_simple_free( pdata->paconn_handle );
+            pdata->paconn_handle = NULL;
+        }
+    }
+#endif
+    if( !pdata->using_pulseaudio)
+        snd_pcm_close( pdata->sound_handle );
 #else
     close(pdata->sound_handle);
 #endif
-    pthread_exit(&errno);
+    pthread_exit( &errno );
 }
-
-
 
